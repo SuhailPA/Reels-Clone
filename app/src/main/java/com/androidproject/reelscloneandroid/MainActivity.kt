eff9c41a1386f98176aecb6f021c52a49ed8cf91
Suhail <suhailabbas23@gmail.com>
2023-02-21 16:41:45 +0530

    Basic funcitonality done

--- app/src/main/java/com/androidproject/reelscloneandroid/MainActivity.kt
+++ app/src/main/java/com/androidproject/reelscloneandroid/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.androidproject.reelscloneandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androidproject.reelscloneandroid.databinding.ActivityMainBinding
+import com.androidproject.reelscloneandroid.model.Video
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var adapter: VideoAdapterClass
+    private val videos = ArrayList<Video>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        videos.add(
+            Video(
+                "Big Buck Bunny",
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            )
+        )
+
+        videos.add(
+            Video(
+                "Elephant Dream",
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+            )
+        )
+
+        videos.add(
+            Video(
+                "For Bigger Blazes",
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+            )
+        )
+        adapter = VideoAdapterClass(this, videos)
+        binding.viewPager2.adapter = adapter
+
+
     }
 }
--- app/src/main/java/com/androidproject/reelscloneandroid/VideoAdapterClass.kt
+++ app/src/main/java/com/androidproject/reelscloneandroid/VideoAdapterClass.kt
@@ -1,10 +1,23 @@
 package com.androidproject.reelscloneandroid
 
 import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.VideoView
 import androidx.recyclerview.widget.RecyclerView
 import com.androidproject.reelscloneandroid.databinding.ListVideoBinding
 import com.androidproject.reelscloneandroid.model.Video
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
 
 class VideoAdapterClass(
     val context: Context,
@@ -15,17 +28,69 @@ class VideoAdapterClass(
         var context: Context
     ) : RecyclerView.ViewHolder(binding.root) {
 
+        private lateinit var exoPlayer: ExoPlayer
+        private lateinit var mediaSource: MediaSource
+
+        fun setVideoPath(url: String) {
+            exoPlayer = ExoPlayer.Builder(context).build()
+            exoPlayer.addListener(object : Player.Listener {
+                override fun onPlayerError(error: PlaybackException) {
+                    super.onPlayerError(error)
+                    Toast.makeText(context, "Can't play this video", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPlaybackStateChanged(playbackState: Int) {
+                    if (playbackState == Player.STATE_BUFFERING) {
+                        binding.progressBarPlayerView.visibility = View.VISIBLE
+                    } else if (playbackState == Player.STATE_READY) {
+                        binding.progressBarPlayerView.visibility = View.GONE
+                    }
+                }
+
+                override fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {
+                    Log.i("ExoplayerCancelReason", reason.toString())
+                    super.onPlayWhenReadyChanged(playWhenReady, reason)
+                }
+            })
+
+            binding.playerView.player = exoPlayer
+
+            exoPlayer.seekTo(0)
+            exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
+
+            val dataSourceFactory = DefaultDataSource.Factory(context)
+
+            mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+                MediaItem.fromUri(
+                    Uri.parse(url)
+                )
+            )
+
+            exoPlayer.setMediaSource(mediaSource)
+            exoPlayer.prepare()
+            if (absoluteAdapterPosition == 0) {
+                exoPlayer.playWhenReady = true
+                exoPlayer.play()
+            }
+
+
+        }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
-        TODO("Not yet implemented")
+        val view = ListVideoBinding.inflate(LayoutInflater.from(context), parent, false)
+        return VideoViewHolder(view, context)
     }
 
     override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val model = videoArrayList[position]
+
+        holder.binding.playerTitle.text = model.title
+        holder.setVideoPath(model.url)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return videoArrayList.size ?: 0
     }
 }
